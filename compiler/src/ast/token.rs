a0bf0d253a169b99c64c1529f435ae83158f24fc
iainquayle <iainquayle@outlook.com>
2024-10-14 03:56:52 +0000

    notes and work on compiler

---
+++ compiler/src/ast/token.rs
@@ -0,0 +1,56 @@
+//this will all be scrapped
+
+pub struct Token {
+	start: usize,
+	kind: TokenType,
+}
+
+impl Token {
+	//pub fn 
+}
+
+pub enum Literal {
+	Int(i64), //probably want to make this something larger, then perhaps box it
+}
+
+pub enum Type {
+	Int(u8),
+}
+
+pub enum TokenType {
+	Whitespace,
+
+	Ident(String),
+	Let,
+	Type,
+
+	LParen,
+	RParen,
+	LBrace,
+	RBrace,
+	LBracket,
+	RBracket,
+	LAngle, //would be nice to not hace these
+	RAngle,
+
+	Comma,
+	Semicolon,
+	Colon,
+
+	Ge,
+	Le,
+	Eq,
+	Ne,
+
+	Not,
+	Neg,
+	Add,
+	Sub,
+	Mul,
+	Div,
+	Mod,
+	And,
+	Or,
+	Xor,
+}
+
---
+++ issues.md
@@ -0,0 +1,15 @@
+
+
+# issues
+
+## definition
+
+where do generics come in, and would it perhaps be something that would just be covered by possible macros?
+would be nice in any case to not need angle braces for generics... or perhaps there some other delimiter that wont cause trivial undecidability issues in the lexer...
+
+## compiler
+
+decide between using haskell, elixir or rust.
+while elixir would be fun, the server will use it, so perhaps tamper with haskell
+
+

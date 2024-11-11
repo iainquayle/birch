a84afc3dd6ef12733fe0c2a3903ed19575600b25
iainquayle <iainquayle@outlook.com>
2024-11-11 07:40:33 +0000

    play with code structure

--- compiler/src/ast.rs
+++ compiler/src/ast.rs
@@ -2,13 +2,12 @@ use token::Token;
 
 
 pub enum Type {
+	None,
 	Primitive(Token),
 	Pointer(Box<Type>), 
 	//not sure how these will be utilized in the syntax, have some kind of escape hatch? or defer all unsafe stuff to using c?
 	//just how will items that use dynamic allocation be created?
 	Array(Box<(Type, usize)>), 
-	Enum(Vec<(String, Option<Type>)>),	
-	Struct(Vec<(String, Type)>),
 	Function {
 		input: Box<Type>,
 		//issue, would work fine for only doing destructuring, but what if the function is given a single primitive type
@@ -16,25 +15,52 @@ pub enum Type {
 		//one is more like react, the other more like every other language
 		output: Box<Type>,
 	},
+	Enum(Vec<(Token, Option<Type>)>),
+	Struct(Vec<(Token, Type)>),
+}
+
+
+pub enum Value {
+	Primitive(Type, Token),
+	//not sure how these will be utilized in the syntax, have some kind of escape hatch? or defer all unsafe stuff to using c?
+	//just how will items that use dynamic allocation be created?
+	Array(Box<(Type, usize)>), 
+	Function {
+		input: Box<Type>,
+		//issue, would work fine for only doing destructuring, but what if the function is given a single primitive type
+		//either, give the inputs a default accessor, ie, in.0, or only allow for the case of complex types
+		//one is more like react, the other more like every other language
+		output: Box<Type>,
+		body: Vec<Statement>,
+	},
+	Struct(Vec<(Token, Value)>),
 }
 
 pub enum Expression {
-	Value(Token),
-	Variable(String),
-	Call { //treat operators as calls for the moment
-		name: Token,
-		args: Vec<Expression>, 
+	Value(Value), //change this
+	Identifier(Token),
+	Call{
+		
+		callee: Box<Expression>, //either a variable, or inlined function
 	},
 }
 
+
+//statement block, includes env capture
+//function is a block, with a env that hasnt been fully resolved
+
+
+
+
 pub enum Statement { 
 	Declare {
-		//mutability
-		name: String,
+		//mutability //this may be less necessary as the fact that mutations dont escape their scope
+		name: Token,
+		generics: Vec<Token>,
 		value: Expression,
 	},
 	Assign {
-		name: String,
+		name: Token,
 		value: Expression,
 	},
 	Return(Expression),
@@ -49,6 +75,11 @@ pub enum Statement {
 //in which case it may just become a constant in the module hierarchy
 
 //also is naming in the correct spot?
+
+//with the move of generics to declarations, can modules somehow become functions?
+//they can return functions that are used, no need for specific import statements
+//only thing would be how to deal with types, and maybe type classes
+//thing about type classes, technically, same thing can be accomplished by passing in functions rather than having global functions essentially.
 
 pub enum Module {
 	//need to allow for generic bounding, which will need a new module type for type classes/traits

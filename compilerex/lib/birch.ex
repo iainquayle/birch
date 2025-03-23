3f3a4dcbf245e6250ae2e788b84608a7568a80b8
iainquayle <iainquayle@outlook.com>
2025-03-23 19:50:30 -0400

    start elixir compiler, python pissed me off

---
+++ compilerex/lib/birch.ex
@@ -0,0 +1,19 @@
+defmodule Birch do
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Compilerex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def test do
+    IO.puts "Hello, world!"
+    hello()
+  end
+end
---
+++ compilerex/test/compilerex_test.exs
@@ -0,0 +1,8 @@
+defmodule CompilerexTest do
+  use ExUnit.Case
+  doctest Compilerex
+
+  test "greets the world" do
+    assert Compilerex.hello() == :world
+  end
+end

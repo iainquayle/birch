abc499bb8afd30f2d03c1a2622222db2c56f454f
iainquayle <iainquayle@outlook.com>
2025-03-23 02:21:23 -0400

    work on parser

--- compiler-py/parse.py
+++ compiler-py/parse.py
@@ -6,9 +6,9 @@ from functools import reduce
 
 from lex import * 
 
+from typing import TypeVar, Generic
 
-
-class Expression(Abstract):
+class AstNode(Abstract):
 	@staticmethod
 	@abstractmethod
 	def eat(tokens: TokenIter) -> Result: 
@@ -17,12 +17,12 @@ class Expression(Abstract):
 type Result = Parsed | None
 @dataclass
 class Parsed:
-	expression: Expression
+	node: AstNode
 	tokens: TokenIter
 
 
 @dataclass
-class Identifier(Expression):
+class Identifier(AstNode):
 	ident: Token 
 	@staticmethod
 	def eat(tokens: TokenIter) -> Result: 
@@ -32,7 +32,7 @@ class Identifier(Expression):
 			return Parsed(Identifier(token), tokens) 
 		return None
 @dataclass
-class Literal(Expression):
+class Literal(AstNode):
 	value: Token
 	@staticmethod
 	def eat(tokens: TokenIter) -> Result: 
@@ -42,7 +42,7 @@ class Literal(Expression):
 			return Parsed(Literal(token), tokens)
 		return None
 
-class Assignee(Abstract):
+class Assignee(AstNode):
 	@staticmethod
 	@abstractmethod
 	def eat(tokens: TokenIter) -> Result:
@@ -55,11 +55,11 @@ class SingleAssignee(Assignee):
 		if (token := next(tokens, None)) is None:
 			return None
 		if isinstance(token, Ident):
-			return Parsed(SingleAssign(token), tokens)
+			return Parsed(SingleAssignee(token), tokens)
 		return None
 @dataclass
 class DestructureAssignee(Assignee):
-	assignees: list[Assignee]
+	assignees: list[tuple[Token, Token | None]] #ident, optional alias
 	@staticmethod
 	def eat(tokens: TokenIter) -> Result:
 		if (token := next(tokens, None)) is None:
@@ -67,117 +67,119 @@ class DestructureAssignee(Assignee):
 		if not isinstance(token, LCurly):
 			return None
 		assignees = []
-		continue_list = True
 		while (token := next(tokens, None)) is not None:
 			if isinstance(token, RCurly):
 				return Parsed(DestructureAssignee(assignees), tokens)
 			assignee = None
+			alias = None
 			if isinstance(token, Ident):
 				assignee = token
-			pass
-		if token is None:
-			return None
-
-			
-			
-
+			else:
+				return None
+			if isinstance((token := next(tokens, None)), As):
+				if isinstance((token := next(tokens, None)), Ident):
+					alias = token
+			if not isinstance((token := next(tokens, None)), Comma):
+				return None
+			assignees.append((assignee, alias))
+		return None
 		
 
 #if the only time where parens are needed are already in a block, then it can be parsed such that if one is found, a new block is parsed
 #rather than looking for it at the beginning of the expression
 @dataclass
-class Block(Expression): 
+class Block(AstNode): 
 	statements: list[Statement] # assignee, type, value
-	return_expression: Expression
+	return_expression: AstNode
 	@staticmethod
 	def eat(tokens: TokenIter) -> Result:
 		while (token := next(tokens, None)) is not None:
 			pass
 @dataclass
-class Function(Expression):
+class Function(AstNode):
 	argument: Token
-	return_expression: Expression
+	return_expression: AstNode
 # statements with types may be nearly ambigous to parse with the current instantiation syntax of enums
 # as it is currently "assign_ident: type", and "variant_ident: expression"
 # the one saving fact is that a full statement is "ident: type = expression", and the equal should differentiate it.
 # the other option is to instantiate enum variants in a syntax like "ident expression"
 @dataclass
-class Struct(Expression):
-	spread: Expression
-	fields: list[tuple[Token, Expression]]
+class Struct(AstNode):
+	spread: AstNode
+	fields: list[tuple[Token, AstNode]]
 @dataclass
-class Array(Expression):
-	spread: Expression
-	values: list[Expression]
+class Array(AstNode):
+	spread: AstNode
+	values: list[AstNode]
 @dataclass
-class Conditional(Expression):
-	conditions: list[tuple[Expression, Expression]]
-	catch_all: Expression 
+class Conditional(AstNode):
+	conditions: list[tuple[AstNode, AstNode]]
+	catch_all: AstNode 
 @dataclass
-class PatternMatch(Expression):
-	expression: Expression
-	cases: list[tuple[Expression, Expression]]
-	catch_all: Expression
+class PatternMatch(AstNode):
+	expression: AstNode
+	cases: list[tuple[AstNode, AstNode]]
+	catch_all: AstNode
 @dataclass
-class Call(Expression):
-	function: Expression
-	argument: Expression
+class Call(AstNode):
+	function: AstNode
+	argument: AstNode
 @dataclass
-class StructAccess(Expression):
-	struct: Expression
+class StructAccess(AstNode):
+	struct: AstNode
 	field: Token
 @dataclass
-class ArrayAccess(Expression):
-	array: Expression
-	index: Expression
+class ArrayAccess(AstNode):
+	array: AstNode
+	index: AstNode
 @dataclass
-class AsType(Expression):
-	expression: Expression
-	reinterpret_type: Expression
+class AsType(AstNode):
+	expression: AstNode
+	reinterpret_type: AstNode
 @dataclass
-class ToType(Expression):
-	expression: Expression
-	target_type: Expression
+class ToType(AstNode):
+	expression: AstNode
+	target_type: AstNode
 @dataclass
-class StructType(Expression):
-	fields: list[tuple[Token, Expression]]
+class StructType(AstNode):
+	fields: list[tuple[Token, AstNode]]
 @dataclass
-class ArrayType(Expression):
-	element_type: Expression
-	length: Expression
+class ArrayType(AstNode):
+	element_type: AstNode
+	length: AstNode
 @dataclass
-class EnumType(Expression):
-	variants: list[tuple[Token, Expression | None]]
+class EnumType(AstNode):
+	variants: list[tuple[Token, AstNode | None]]
 @dataclass
-class FunctionType(Expression):
-	argument_type: Expression
-	return_type: Expression
+class FunctionType(AstNode):
+	argument_type: AstNode
+	return_type: AstNode
 @dataclass
-class PrimitiveType(Expression):
+class PrimitiveType(AstNode):
 	prim_type: Token
 @dataclass
-class Mul(Expression):
-	left: Expression
-	right: Expression
+class Mul(AstNode):
+	left: AstNode
+	right: AstNode
 @dataclass
-class Add(Expression):
-	left: Expression
-	right: Expression
+class Add(AstNode):
+	left: AstNode
+	right: AstNode
 @dataclass
-class Logical(Expression):
-	left: Expression
-	right: Expression
+class Logical(AstNode):
+	left: AstNode
+	right: AstNode
 @dataclass
-class Compare(Expression):
-	left: Expression
-	right: Expression
+class Compare(AstNode):
+	left: AstNode
+	right: AstNode
 
 
 @dataclass
 class Statement:
 	assignee: Assignee
-	value_type: Expression | None
-	value: Expression
+	value_type: AstNode | None
+	value: AstNode
 	@staticmethod
 	def eat(tokens: TokenIter) -> Result:
 		if (assignee := Identifier.eat(tokens)) is None:
@@ -190,7 +192,7 @@ class Statement:
 	
 
 
-def longest_parse(tokens: TokenIter, possible_expressions: list[type[Expression]] | None = None) -> Result:
+def longest_parse(tokens: TokenIter, possible_expressions: list[type[AstNode]] | None = None) -> Result:
 	if possible_expressions is None:
 		possible_expressions = [
 			Identifier,

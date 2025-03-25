41db29f5563b569ce311c3b892d62eb7dd2db9a5
iainquayle <iainquayle@outlook.com>
2025-03-25 00:46:42 -0400

    start parser

--- compiler-ex/lib/birch.ex
+++ compiler-ex/lib/birch.ex
@@ -8,13 +8,19 @@ defmodule Birch do
 
   """
 
-  def test do
+  def test_lexer do
     #source = String.graphemes("f: {x: u32, y: u32} = {x, y} => x + y")
     source = String.graphemes("let ab + c 10")
     tokens = Birch.Lexer.tokenize(source, %Birch.Lexer.Position{})
     for {token, position} <- tokens do
-
       IO.puts("#{inspect(token)} at #{position.index}")
     end
+  end
+
+  def test_binding_parser do
+    source = String.graphemes("{x, {y, z}}")
+    tokens = Birch.Lexer.tokenize(source, %Birch.Lexer.Position{})
+    parsed = Birch.Parser.parse_binding(tokens)
+    IO.inspect(parsed)
   end
 end
--- compiler-ex/lib/parser.ex
+++ compiler-ex/lib/parser.ex
@@ -8,30 +8,36 @@ defmodule Birch.Parser do
 
   def parse_binding(tokens) do
     case tokens do
-      [] -> nil
+      [] -> {:error, "No tokens to parse"} 
       [token | rest] -> case token do
         {{:identifier, _}, _} -> {{:binding, token}, rest} 
         {:l_curly, _} -> parse_destructure_binding([], rest)
-        _ -> nil
+        _ -> {:error, "Invalid token for binding"} 
       end
     end
   end
 
   defp parse_destructure_binding(bindings, tokens) do
     case tokens do
-      [] -> nil
-      [token | rest] -> case token do
+      [] -> {:error, "No tokens to parse"} 
+      [token | rest] -> result = case token do
+        {:l_curly, _} -> result = parse_destructure_binding([], rest)
+          case result do
+            {:error, _} -> result 
+            {nested_bindings, rest} -> parse_destructure_binding([nested_bindings | bindings], rest)
+            _ -> {:error, "Invalid result from nested destructure binding"}
+          end
         {:comma, _} -> parse_destructure_binding(bindings, rest)
         {{:identifier, _}, _} -> case rest do
           [{:as, _} | rest] -> [alias_token | rest] = rest
             case alias_token do
               {{:identifier, _}, _} -> parse_destructure_binding([{token, alias_token} | bindings], rest)
-              _ -> nil
+              _ -> {:error, "Invalid token for alias"} 
             end
           _ -> parse_destructure_binding([{token, nil} | bindings], rest)
         end
         {:r_curly, _} -> {{:destructure_binding, bindings}, rest}
-        _ -> nil
+        _ -> {:error, "Invalid token for destructuring binding"}
       end
     end
   end
